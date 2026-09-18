a4f6c88b4ee9cbc77552ccc9ec72a2e09307454f
sirhellme <sirhellme@gmail.com>
2026-09-19 07:01:04 +0800

    .

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 import warnings
+import lightkurve as lk
+import matplotlib.pyplot as py
 
 warnings.filterwarnings(
     "ignore",
     message=".*tpfmodel submodule is not available.*"
 )
-
-import lightkurve as lk
 
 search = lk.search_lightcurve(
     "Kepler-227 b",
