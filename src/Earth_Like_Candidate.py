f3116e27e0b2b08caf2dc40669417a97fd0234ca
sirhellme <sirhellme@gmail.com>
2026-09-23 03:59:41 +0800

    organize my data

--- src/Earth_Like_Candidate.py
+++ src/Earth_Like_Candidate.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-file = "/home/hell/Projects/AKCTE Komputasi/dataset.csv"
+file = "/yourdatasets.csv"
 
 df =pd.read_csv(file)
 
