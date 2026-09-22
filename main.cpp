ae89da862776c8115130390abad9e8748ff3838b
sirhellme <sirhellme@gmail.com>
2026-09-23 03:11:40 +0800

    orginize my folder

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <limits>
+#include <numeric>
+#include <iomanip>
+
+struct LightCurve {
+    /* data */
+    std::vector<double> time;
+    std::vector<double> flux;
+};
